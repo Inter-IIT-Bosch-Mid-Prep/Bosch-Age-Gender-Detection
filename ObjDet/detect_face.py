861542b24314529e3a583390ff56adc37d637d52
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2022-03-10 11:09:20 +0530

    Objdetection

--- ObjDet/detect_face.py
+++ ObjDet/detect_face.py
@@ -145,16 +145,3 @@ def detect_one(model, image_path, device):
 
     cv2.imwrite('result.jpg', orgimg)
 
-
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp5/weights/last.pt', help='model.pt path(s)')
-    parser.add_argument('--image', type=str, default='data/images/test.jpg', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    opt = parser.parse_args()
-    print(opt)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model(opt.weights, device)
-    detect_one(model, opt.image, device)
